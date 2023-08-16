8f6c20f0483a4a6d5caee562b06f22187331ae94
Maurice Berk <maurice@mauriceberk.com>
2023-08-16 18:03:20 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="shin",
-    version="0.0.2",
+    version="0.1.0",
     description="Python implementation of Shin's method for calculating implied probabilities from bookmaker odds",
     long_description=long_description,
     long_description_content_type="text/markdown",
