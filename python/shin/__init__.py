02fa45bbca2dab6269eac5103c73bfb50e440178
Maurice Berk <maurice@mauriceberk.com>
2023-08-17 08:36:43 +0100

    Implement Rust optimiser

--- python/shin/__init__.py
+++ python/shin/__init__.py
@@ -3,6 +3,9 @@ from math import sqrt
 from typing import Any, Union
 
 
+from .shin import optimise as _optimise_rust
+
+
 def _optimise(
     inverse_odds: list[float],
     sum_inverse_odds: float,
@@ -32,12 +35,15 @@ def calculate_implied_probabilities(
     max_iterations: int = 1000,
     convergence_threshold: float = 1e-12,
     only_return_probabilities: bool = False,
+    force_python_optimiser: bool = False,
 ) -> Union[dict[str, Any], list[float]]:
     if len(odds) < 2:
         raise ValueError("len(odds) must be >= 2")
 
     if any(o < 1 for o in odds):
         raise ValueError("All odds must be >= 1")
+
+    optimise = _optimise if force_python_optimiser else _optimise_rust
 
     n = len(odds)
     inverse_odds = [1.0 / o for o in odds]
@@ -51,7 +57,7 @@ def calculate_implied_probabilities(
         delta = 0
         iterations = 0
     else:
-        z, delta, iterations = _optimise(
+        z, delta, iterations = optimise(
             inverse_odds=inverse_odds,
             sum_inverse_odds=sum_inverse_odds,
             n=n,
