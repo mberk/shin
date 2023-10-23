1d06e56a854c7b213aa3fb5b488dbf9136ace0bb
Maurice Berk <maurice@mauriceberk.com>
2023-10-23 07:13:45 +0100

    Replace only_return_probabilities with full_output

--- python/shin/__init__.py
+++ python/shin/__init__.py
@@ -20,7 +20,7 @@ def _optimise(
         z0 = z
         z = (
             sum(
-                sqrt(z**2 + 4 * (1 - z) * io**2 / sum_inverse_odds)
+                sqrt(z ** 2 + 4 * (1 - z) * io ** 2 / sum_inverse_odds)
                 for io in inverse_odds
             )
             - 2
@@ -34,7 +34,7 @@ def calculate_implied_probabilities(
     odds: Collection[float],
     max_iterations: int = 1000,
     convergence_threshold: float = 1e-12,
-    only_return_probabilities: bool = False,
+    full_output: bool = False,
     force_python_optimiser: bool = False,
 ) -> Union[dict[str, Any], list[float]]:
     if len(odds) < 2:
@@ -51,8 +51,8 @@ def calculate_implied_probabilities(
 
     if n == 2:
         diff_inverse_odds = inverse_odds[0] - inverse_odds[1]
-        z = ((sum_inverse_odds - 1) * (diff_inverse_odds**2 - sum_inverse_odds)) / (
-            sum_inverse_odds * (diff_inverse_odds**2 - 1)
+        z = ((sum_inverse_odds - 1) * (diff_inverse_odds ** 2 - sum_inverse_odds)) / (
+            sum_inverse_odds * (diff_inverse_odds ** 2 - 1)
         )
         delta = 0
         iterations = 0
@@ -66,16 +66,16 @@ def calculate_implied_probabilities(
         )
 
     p = [
-        (sqrt(z**2 + 4 * (1 - z) * io**2 / sum_inverse_odds) - z) / (2 * (1 - z))
+        (sqrt(z ** 2 + 4 * (1 - z) * io ** 2 / sum_inverse_odds) - z) / (2 * (1 - z))
         for io in inverse_odds
     ]
 
-    if only_return_probabilities:
-        return p
-    else:
+    if full_output:
         return {
             "implied_probabilities": p,
             "iterations": iterations,
             "delta": delta,
             "z": z,
         }
+    else:
+        return p
--- tests/test_shin.py
+++ tests/test_shin.py
@@ -13,25 +13,23 @@ def test_calculate_implied_probabilities():
     with pytest.raises(ValueError):
         shin.calculate_implied_probabilities([0.9, 0.1])
 
+    result = shin.calculate_implied_probabilities([2.6, 2.4, 4.3], full_output=True)
+
+    assert pytest.approx(0.3729941) == result["implied_probabilities"][0]
+    assert pytest.approx(0.4047794) == result["implied_probabilities"][1]
+    assert pytest.approx(0.2222265) == result["implied_probabilities"][2]
+    assert pytest.approx(0.01694251) == result["z"]
+
+    result = shin.calculate_implied_probabilities(
+        [2.6, 2.4, 4.3], full_output=True, force_python_optimiser=True
+    )
+
+    assert pytest.approx(0.3729941) == result["implied_probabilities"][0]
+    assert pytest.approx(0.4047794) == result["implied_probabilities"][1]
+    assert pytest.approx(0.2222265) == result["implied_probabilities"][2]
+    assert pytest.approx(0.01694251) == result["z"]
+
     result = shin.calculate_implied_probabilities([2.6, 2.4, 4.3])
-
-    assert pytest.approx(0.3729941) == result["implied_probabilities"][0]
-    assert pytest.approx(0.4047794) == result["implied_probabilities"][1]
-    assert pytest.approx(0.2222265) == result["implied_probabilities"][2]
-    assert pytest.approx(0.01694251) == result["z"]
-
-    result = shin.calculate_implied_probabilities(
-        [2.6, 2.4, 4.3], force_python_optimiser=True
-    )
-
-    assert pytest.approx(0.3729941) == result["implied_probabilities"][0]
-    assert pytest.approx(0.4047794) == result["implied_probabilities"][1]
-    assert pytest.approx(0.2222265) == result["implied_probabilities"][2]
-    assert pytest.approx(0.01694251) == result["z"]
-
-    result = shin.calculate_implied_probabilities(
-        [2.6, 2.4, 4.3], only_return_probabilities=True
-    )
     assert pytest.approx(0.3729941) == result[0]
     assert pytest.approx(0.4047794) == result[1]
     assert pytest.approx(0.2222265) == result[2]
@@ -39,7 +37,7 @@ def test_calculate_implied_probabilities():
     odds = [1.5, 2.74]
     inverse_odds = [1 / o for o in odds]
     sum_inverse_odds = sum(inverse_odds)
-    result = shin.calculate_implied_probabilities(odds)
+    result = shin.calculate_implied_probabilities(odds, full_output=True)
 
     assert result["iterations"] == 0
     assert result["delta"] == 0
