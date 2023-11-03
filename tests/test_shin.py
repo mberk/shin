dfe60b25e22e434e68981de8c558a016dfc28dec
Maurice Berk <maurice@mauriceberk.com>
2023-11-03 08:17:16 +0000

    Optionally operate on Mappings

--- tests/test_shin.py
+++ tests/test_shin.py
@@ -34,6 +34,25 @@ def test_calculate_implied_probabilities():
     assert pytest.approx(0.4047794) == result[1]
     assert pytest.approx(0.2222265) == result[2]
 
+    result = shin.calculate_implied_probabilities(
+        {"HOME": 2.6, "AWAY": 2.4, "DRAW": 4.3}
+    )
+    assert {
+        "HOME": pytest.approx(0.3729941),
+        "AWAY": pytest.approx(0.4047794),
+        "DRAW": pytest.approx(0.2222265),
+    } == result
+
+    result = shin.calculate_implied_probabilities(
+        {"HOME": 2.6, "AWAY": 2.4, "DRAW": 4.3}, full_output=True
+    )
+    assert {
+        "HOME": pytest.approx(0.3729941),
+        "AWAY": pytest.approx(0.4047794),
+        "DRAW": pytest.approx(0.2222265),
+    } == result["implied_probabilities"]
+    assert pytest.approx(0.01694251) == result["z"]
+
     odds = [1.5, 2.74]
     inverse_odds = [1 / o for o in odds]
     sum_inverse_odds = sum(inverse_odds)
