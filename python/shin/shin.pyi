2d588fe793b159e2025c08c9c1a4ffb618fb6599
Peter Schutt <peter.github@proton.me>
2024-01-31 10:44:23 +1000

    Adds typesafety test for `shin.shin` stub.

--- python/shin/shin.pyi
+++ python/shin/shin.pyi
@@ -2,7 +2,6 @@ def optimise(
     inverse_odds: list[float],
     sum_inverse_odds: float,
     n: int,
-    max_iterations: int = 1000,
-    convergence_threshold: float = 1e-12,
-) -> tuple[float, float, float]:
-    ...
+    max_iterations: int = ...,
+    convergence_threshold: float = ...,
+) -> tuple[float, float, float]: ...
